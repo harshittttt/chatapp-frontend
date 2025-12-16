e646dce77b5905539bb7d94413f4c40a047dde94
Harshit Saini <harshitsaini0904@gmail.com>
2025-12-16 23:44:30 +0530

    trying headers

--- src/socket.jsx
+++ src/socket.jsx
@@ -7,7 +7,15 @@ const SocketContext = createContext();
 const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io(server, { withCredentials: true }), []);
+  const socket = useMemo(() => {
+    const token = localStorage.getItem("chattu-token");
+    return io(server, { 
+      withCredentials: true,
+      auth: {
+        token: token
+      }
+    });
+  }, []);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
